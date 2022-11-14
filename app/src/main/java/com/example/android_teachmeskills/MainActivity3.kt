cfaa47401afaf8a5f3c53ed0e16e438f23278a8f
marculevichd <114149286+marculevichd@users.noreply.github.com>
2022-11-14 18:47:42 +0300

    lesson 17

--- app/src/main/java/com/example/android_teachmeskills/MainActivity3.kt
+++ app/src/main/java/com/example/android_teachmeskills/MainActivity3.kt
@@ -1,11 +1,20 @@
 package com.example.android_teachmeskills
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+
+        val btnGoToActivity3 = findViewById<Button>(R.id.btnGoToActivity1)
+
+        btnGoToActivity3.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
